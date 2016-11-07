da2707abb3411a360b6765d5e3f7af8358027e03
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-11-07 18:44:32 +0100

    cosmetic in watchers.sh

--- oai-skype/watchers.sh
+++ oai-skype/watchers.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+slice=onelab.inria.oai.oai_build
+
 for arg in "$@"; do
     fitname=fit$(printf "%02d" $arg)
-    echo $fitname
-    ssh -X onelab.inria.oai.oai_build@faraday.inria.fr ssh -X root@$fitname xterm &
+    echo Starting xterm on $fitname as slice $slice
+    ssh -X $slice@faraday.inria.fr ssh -X root@$fitname xterm &
 done
