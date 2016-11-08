992de5ad7f842f8f6610ab1e0294c40c5ef0258d
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-11-08 11:08:47 +0100

    nicer watchers

--- oai-skype/watchers.sh
+++ oai-skype/watchers.sh
@@ -2,8 +2,11 @@
 
 slice=onelab.inria.oai.oai_build
 
+font="-*-fixed-medium-*-*-*-20-*-*-*-*-*-*-*"
+xterm_args="-fn $font -bg wheat -geometry 90x10"
+
 for arg in "$@"; do
     fitname=fit$(printf "%02d" $arg)
     echo Starting xterm on $fitname as slice $slice
-    ssh -X $slice@faraday.inria.fr ssh -X root@$fitname xterm &
+    ssh -X $slice@faraday.inria.fr ssh -X root@$fitname xterm "$xterm_args" &
 done
