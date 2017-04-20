d9151bba7b4b637eef5e9a2ef39374055ea1de08
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2017-04-20 11:06:19 +0200

    offer the choice of all channels in visumap

--- radiomap/acquiremap.py
+++ radiomap/acquiremap.py
@@ -11,8 +11,10 @@ from apssh import SshNode, LocalNode, SshJob
 from apssh import Run, RunScript, Pull
 from apssh import TimeColonFormatter
 
+# helpers
 from processmap import Aggregator
 from listofchoices import ListOfChoices
+from frequencies import channel_frequency
 
 ##########
 default_gateway_hostname  = 'faraday.inria.fr'
@@ -31,25 +33,8 @@ default_phy_rate          = 1
 choices_tx_power          = ['500', '1400']
 default_tx_power          = 1400
 
-# source
-# http://www.radio-electronics.com/info/wireless/wi-fi/80211-channels-number-frequencies-bandwidth.php
-from collections import OrderedDict
-channel_frequencies = OrderedDict([
-    (1,   2412), (2,   2417), (3,   2422), (4,   2427),
-    (5,   2432), (6,   2437), (7,   2442), (8,   2447),
-    (9,   2452), (10,  2457), (11,  2462), (12,  2467),
-    (13,  2472), (14,  2484),
-    (36,  5180), (40,  5200), (44,  5220), (48,  5240),
-    (52,  5260), (56,  5280), (60,  5300), (64,  5320),
-    (100, 5500), (104, 5520), (108, 5540), (112, 5560),
-    (116, 5580), (120, 5600), (124, 5620), (128, 5640),
-    (132, 5660), (136, 5680), (140, 5700), (149, 5745),
-    (153, 5765), (157, 5785), (161, 5805), (165, 5825),
-])
-    
-for k, v in channel_frequencies.items():
-    channel_frequencies[k] = str(v)
-choices_frequency         = list(channel_frequencies.values())
+# we'd rather provide a channel number than a frequency 
+choices_frequency         = list(str(f) for f in channel_frequency.values())
 # this is channel 1
 default_frequency         = 2412
 
