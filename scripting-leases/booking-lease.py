c3965866ef1925c433acd1a2a86e343bb211cb81
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2019-09-05 13:06:24 +0200

    Adding command line options:  --help  --status  --book
    Next steps could be to pass all AUTH and booking parameters by command lines
    
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- scripting-leases/booking-lease.py
+++ scripting-leases/booking-lease.py
@@ -6,18 +6,26 @@ account = "thierry.parmentelat@inria.fr"
 password = "your password here"
 slicename = "inria_r2lab.nightly"
 
-readable_from  = "2019-09-03T04:00:00"
-readable_until = "2019-09-03T05:00:00"
+readable_from  = "2019-09-05T04:00:00"
+readable_until = "2019-09-05T05:00:00"
 
 # end of editable part
 
 
 #######
 
+import sys
+import re
 import time
 from datetime import datetime
 from xmlrpc.client import ServerProxy
 
+def Usage():
+    print('------------------------------------------------------------')
+    print('Usage: python3 booking-lease.py [options]')
+    print('  --help    Show this help.')
+    print('  --status  Show the current and upcoming bookings.')
+    print('  --book    Book a slice.')
 
 ####### only utilities to use pretty timestamps instead of raw epochs
 
@@ -30,6 +38,23 @@ def readable_to_epoch(string):
 def epoch_to_readable(epoch):
     return f"{time.strftime(timeformat, time.localtime(epoch))}"
 
+book = False
+getStatus = False
+
+argvs = sys.argv
+argc = len(argvs)
+while len(argvs) > 1:
+    myArgv = argvs.pop(1)   # 0th is this file's name
+    if re.match('^\-\-help$', myArgv, re.IGNORECASE):
+        Usage()
+        sys.exit(0)
+    elif re.match('^\-\-status$', myArgv, re.IGNORECASE):
+        getStatus = True
+    elif re.match('^\-\-book$', myArgv, re.IGNORECASE):
+        book = True
+    else:
+        Usage()
+        sys.exit('Invalid Parameter: ' + myArgv)
 
 ### 
 r2lab_url = "https://r2labapi.inria.fr/PLCAPI/"
@@ -45,8 +70,6 @@ auth = {
 # where to send API calls
 proxy = ServerProxy(r2lab_url, allow_none=True)
 
-
-
 def print_future_leases():
 
     leases = proxy.GetLeases(auth, {'day': 0})
@@ -56,9 +79,8 @@ def print_future_leases():
               f"→ {epoch_to_readable(lease['t_until'])} "
               f" by slice {lease['name']}")
 
-print_future_leases()
-
-
+if getStatus:
+    print_future_leases()
 
 def book_lease(t_from, t_until, slicename):
 
@@ -74,8 +96,9 @@ def book_lease(t_from, t_until, slicename):
     else:
         print(f"new lease OK, id={retcod['new_ids'][0]}")
 
-book_lease(
-    readable_to_epoch(readable_from),
-    readable_to_epoch(readable_until),
-    slicename,
-)
+if book:
+    book_lease(
+        readable_to_epoch(readable_from),
+        readable_to_epoch(readable_until),
+        slicename,
+    )
